9e749ffe9a3f1d2e836b3594f1dc87f464d4bb83
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:51:44 -0600

    Rewriting as Laminas Project package

--- test/InputFilter/RestService/PostInputFilterTest.php
+++ test/InputFilter/RestService/PostInputFilterTest.php
@@ -1,14 +1,16 @@
 <?php
+
 /**
- * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @see       https://github.com/laminas-api-tools/api-tools-admin for the canonical source repository
+ * @copyright https://github.com/laminas-api-tools/api-tools-admin/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas-api-tools/api-tools-admin/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZFTest\Apigility\Admin\InputFilter\RestService;
+namespace LaminasTest\ApiTools\Admin\InputFilter\RestService;
 
+use Laminas\ApiTools\Admin\InputFilter\RestService\PostInputFilter;
+use Laminas\InputFilter\Factory;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\InputFilter\Factory;
-use ZF\Apigility\Admin\InputFilter\RestService\PostInputFilter;
 
 class PostInputFilterTest extends TestCase
 {
@@ -16,7 +18,7 @@ class PostInputFilterTest extends TestCase
     {
         $factory = new Factory();
         return $factory->createInputFilter([
-            'type' => 'ZF\Apigility\Admin\InputFilter\RestService\PostInputFilter',
+            'type' => 'Laminas\ApiTools\Admin\InputFilter\RestService\PostInputFilter',
         ]);
     }
 
