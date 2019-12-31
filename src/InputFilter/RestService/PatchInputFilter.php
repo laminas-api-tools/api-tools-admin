7bcbbb76d63b0f7f8d2095573d17eb7ea34b93d6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:50:43 -0600

    Rewriting as Laminas Project package

--- src/InputFilter/RestService/PatchInputFilter.php
+++ src/InputFilter/RestService/PatchInputFilter.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
- * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @see       https://github.com/laminas-api-tools/api-tools-admin for the canonical source repository
+ * @copyright https://github.com/laminas-api-tools/api-tools-admin/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas-api-tools/api-tools-admin/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZF\Apigility\Admin\InputFilter\RestService;
+namespace Laminas\ApiTools\Admin\InputFilter\RestService;
 
 class PatchInputFilter extends PostInputFilter
 {
@@ -59,7 +61,7 @@ class PatchInputFilter extends PostInputFilter
             'name' => 'selector',
             'required' => false,
             'allow_empty' => true,
-            'error_message' => 'The Content Negotiation Selector must be a valid, defined zf-content-negotiation selector',
+            'error_message' => 'The Content Negotiation Selector must be a valid, defined api-tools-content-negotiation selector',
         ));
 
         $this->add(array(
@@ -68,7 +70,7 @@ class PatchInputFilter extends PostInputFilter
             'allow_empty' => true,
             'continue_if_empty' => true,
             'validators' => array(
-                array('name' => 'Zend\Validator\Digits')
+                array('name' => 'Laminas\Validator\Digits')
             ),
             'error_message' => 'The Page Size must be an integer',
         ));
@@ -78,7 +80,7 @@ class PatchInputFilter extends PostInputFilter
             'allow_empty' => true,
             'continue_if_empty' => true,
             'validators' => array(
-                array('name' => 'ZF\Apigility\Admin\InputFilter\Validator\HttpMethodArrayValidator')
+                array('name' => 'Laminas\ApiTools\Admin\InputFilter\Validator\HttpMethodArrayValidator')
             ),
             'error_message' => 'The Collection HTTP Methods must be an array of valid HTTP methods',
         ));
@@ -88,7 +90,7 @@ class PatchInputFilter extends PostInputFilter
             'allow_empty' => true,
             'continue_if_empty' => true,
             'validators' => array(
-                array('name' => 'ZF\Apigility\Admin\InputFilter\Validator\HttpMethodArrayValidator')
+                array('name' => 'Laminas\ApiTools\Admin\InputFilter\Validator\HttpMethodArrayValidator')
             ),
             'error_message' => 'The Entity HTTP Methods must be an array of valid HTTP methods',
         ));
